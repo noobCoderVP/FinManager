b2e93289107ebd1c1c8f58b50f6140480d24a553
noobcodervp <vaibhavpatel02892@gmail.com>
2023-03-23 11:01:31 +0530

    Added everything

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: false,
     env: {
-        BASE_URL: "https://finmanager-backend-production.up.railway.app/",
+        BASE_URL: "https://finmanager-backend-production.up.railway.app",
         WEB_URL: "http://localhost:3000",
     },
     images: {
